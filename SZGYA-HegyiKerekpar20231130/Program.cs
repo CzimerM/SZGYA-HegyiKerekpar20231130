54ecc624b76cc05eee96a1001957eabfac8a78b3
Csizmadia Márk <kindmysticaltree@gmail.com>
2023-11-30 09:13:54 +0100

    Add project files.

---
+++ SZGYA-HegyiKerekpar20231130/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace SZGYA_HegyiKerekpar20231130
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+
+            StreamReader sr = new StreamReader("../../../src/bukkm2019.txt",Encoding.UTF8);
+            _ = sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                versenyzok.Add(new Versenyzo(sr.ReadLine()));
+            }
+            sr.Close();
+
+            const int osszVersenyzo = 691;
+            Console.WriteLine($"4. feladat: Versenytávot nem teljesítők: {((osszVersenyzo - versenyzok.Count) / (float)osszVersenyzo) * 100}%");
+            Console.WriteLine($"5. feladat: Női versenyzők száma a rövid távú versenyen: {versenyzok.Where(v => v.Kategoria.Last() == 'n' && v.Tav == "Rövid").ToList().Count}fő");
+            Console.WriteLine($"6. feladat: {(versenyzok.Any(v => v.Ido > new TimeSpan(6,0,0)) ? "Van" : "Nincs")} ilyen versenyző");
+            Console.WriteLine($"7. feladat: A felnőtt férfi (ff) kategória győztese rövid távon\n{versenyzok.Where(v => v.Kategoria == "ff" && v.Tav == "Rövid").MinBy(v => v.Ido)}");
+            Console.WriteLine("8. feladat: Statisztika");
+            var f8stat = versenyzok
+                .Where(v => v.Kategoria.Last() == 'f')
+                .GroupBy(v => v.Kategoria)
+                .ToDictionary(k => k.Key, v => v.ToList().Count);
+            foreach (var kv in f8stat)
+            {
+                Console.WriteLine($"\t{kv.Key} - {kv.Value}fő");
+            }
+        }
+    }
+}
---
+++ SZGYA-HegyiKerekpar20231130/Versenyzo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_HegyiKerekpar20231130
+{
+    class Versenyzo
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev {  get; set; }
+        public string Egyesulet { get; set; }
+        public TimeSpan Ido { get; set; }
+        public string Tav => _tav.Tav;
+        private Versenytav _tav;
+
+        public Versenyzo(string sor)
+        {
+            string[] adatok = sor.Split(';');
+            this.Rajtszam = adatok[0];
+            this.Kategoria = adatok[1];
+            this.Nev = adatok[2];
+            this.Egyesulet = adatok[3];
+            this.Ido = TimeSpan.Parse(adatok[4]);
+            this._tav = new Versenytav(this.Rajtszam);
+        }
+
+        public override string ToString()
+        {
+            return $"\tRajtszám: {this.Rajtszam}\n\tNév: {this.Nev}\n\tEgyesület: {this.Egyesulet}\n\tIdő: {this.Ido}";
+        }
+    }
+}
